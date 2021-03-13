a43bfeab50c963a7e4bede906da368c23981037d
chaiwat2021 <79843590+chaiwat2021@users.noreply.github.com>
2021-03-13 21:30:15 +0700

    Testing in Lambda and Delegate

---
+++ CSharpTesting/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Count80();
+            TestDelegate();
+            TestLambda();
+
+            Console.ReadKey();
+        }
+
+        private static void Count80()
+        {
+            // Data source.
+            int[] scores = { 90, 71, 82, 93, 75, 82 };
+
+            // The call to Count forces iteration of the source
+            int highScoreCount = scores.Where(n => n > 80).Count();
+
+            Console.WriteLine("{0} scores are greater than 80", highScoreCount);
+
+            // Outputs: 4 scores are greater than 80
+        }
+
+        private static void TestDelegate()
+        {
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(5));
+        }
+
+        private static void TestLambda()
+        {
+            int[] number = { 2, 3, 4, 5 };
+            var squaredNumbers = number.Select(x => x * x);
+            Console.WriteLine(string.Join(" ", squaredNumbers));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# csharp_testing
-C# Testing
+## C# Testing
+**Developer Tools**
+- Visual Studio Community 2017
+- .Net Core 2.2.10 SDK
+
+**Knowledges**
+- How to use lambda expressions in a query from [Visual Studio Page](https://docs.microsoft.com/th-th/dotnet/csharp/programming-guide/statements-expressions-operators/how-to-use-lambda-expressions-in-a-query)
+- Lambda expressions (C# reference) from [Visual Studio Page](https://docs.microsoft.com/th-th/dotnet/csharp/language-reference/operators/lambda-expressions)
